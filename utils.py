71b49ec32b83e05d7a6c9933fa7fc01e763ce67e
github-account92 <j_johannsmeier@gmx.de>
2019-01-22 11:49:31 +0100

    we notebook now

--- utils.py
+++ utils.py
@@ -1,4 +1,56 @@
 import tensorflow as tf
+import numpy as np
+
+
+def make_mnist_iter(batch_size, train=True):
+    pl_imgs = tf.placeholder(tf.float32, [None, 784])
+    pl_lbls = tf.placeholder(tf.int32, [None])
+    data = tf.data.Dataset.from_tensor_slices((pl_imgs, pl_lbls))
+    if train:
+        data = data.apply(tf.data.experimental.shuffle_and_repeat(60000))
+    data = data.batch(batch_size)
+    return data.make_initializable_iterator(), pl_imgs, pl_lbls
+
+
+def init_mnist(sess, iterator, pl_imgs, pl_lbls, train=True, normalize=True,
+               binarize=False):
+    if train:
+        imgs = np.load("data/mnist_train_imgs.npy")
+        lbls = np.load("data/mnist_train_lbls.npy")
+    else:
+        imgs = np.load("data/mnist_test_imgs.npy")
+        lbls = np.load("data/mnist_test_lbls.npy")
+    lbls = lbls.astype(np.int32)
+    if normalize:
+        imgs = imgs.astype(np.float32) / 255
+    if binarize:
+        if not normalize:
+            raise ValueError("Binarization not implemented for unnormalized "
+                             "data.")
+        imgs = np.around(imgs)
+    sess.run(iterator.initializer, feed_dict={pl_imgs: imgs, pl_lbls: lbls})
+
+
+def mnist_eager(batch_size, train=True, normalize=True, binarize=False):
+    if train:
+        imgs = np.load("data/mnist_train_imgs.npy")
+        lbls = np.load("data/mnist_train_lbls.npy")
+    else:
+        imgs = np.load("data/mnist_test_imgs.npy")
+        lbls = np.load("data/mnist_test_lbls.npy")
+    lbls = lbls.astype(np.int32)
+    if normalize:
+        imgs = imgs.astype(np.float32) / 255
+    if binarize:
+        if not normalize:
+            raise ValueError("Binarization not implemented for unnormalized "
+                             "data.")
+        imgs = np.around(imgs)
+    data = tf.data.Dataset.from_tensor_slices((imgs, lbls))
+    if train:
+        data = data.apply(tf.data.experimental.shuffle_and_repeat(60000))
+    data = data.batch(batch_size)
+    return data
 
 
 def matvec(mat, vec):
@@ -14,6 +66,9 @@ def matvec(mat, vec):
     return tf.squeeze(tf.matmul(mat, vec[:, tf.newaxis]), axis=-1)
 
 
+################################################################################
+# RBM
+################################################################################
 def repeated_gibbs(init_sample, n_iters, gibbs_update_fn, **guf_kwargs):
     """Repeatedly apply Gibbs updates for a given number of iterations.
 
@@ -80,3 +135,47 @@ def energy_rbm(v, h, w_vh, b_v, b_h):
     """
     return (-matvec(v, b_v) - matvec(h, b_h) -
             tf.reduce_sum(tf.matmul(v, w_vh) * h, axis=-1))
+
+
+################################################################################
+# MMD
+################################################################################
+def compute_kernel(x, y, sigma_sqr):
+    """Compute pairwise similarity measure between two batches of vectors.
+    Parameters:
+        x: n x d tensor of floats.
+        y: Like x.
+        sigma_sqr: Variance for the Gaussian kernel.
+    Returns:
+        n x n tensor where element i, j contains similarity between element i
+        in x and element j in y.
+    """
+    x_broadcast = x[:, tf.newaxis, :]
+    y_broadcast = y[tf.newaxis, :, :]
+    return tf.exp(
+        -tf.reduce_mean(tf.squared_difference(x_broadcast, y_broadcast),
+                        axis=2) / sigma_sqr)
+
+
+def compute_mmd(x, y, sigma_sqr=None):
+    """Compute MMD between two batches of vectors.
+    Parameters:
+        x: n x d tensor of floats.
+        y: Like x.
+        sigma_sqr: Variance for the Gaussian kernel. Can be None to choose
+                   default value based on number of dimensions, a scalar, or
+                   a list of scalars to add multiple kernels.
+    Returns:
+        Scalar MMD value.
+    """
+    if sigma_sqr is None:
+        sigma_sqr = tf.cast(x.shape.as_list()[1], tf.float32)
+    elif type(sigma_sqr) is float or type(sigma_sqr) is int:
+        sigma_sqr = [sigma_sqr]
+    total = 0
+    for sig in sigma_sqr:
+        x_kernel = compute_kernel(x, x, sig)
+        y_kernel = compute_kernel(y, y, sig)
+        xy_kernel = compute_kernel(x, y, sig)
+        total += tf.reduce_mean(x_kernel + y_kernel - 2 * xy_kernel)
+    return total
