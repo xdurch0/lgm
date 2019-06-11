d72b230d5f8624d8fdb5d16b01645c45aa9a6971
xdurch0 <j_johannsmeier@gmx.de>
2019-06-11 14:52:10 +0200

    add 'very own' LSTM cell implementation

--- utils/custom_layers.py
+++ utils/custom_layers.py
@@ -3,6 +3,13 @@ import logging
 import tensorflow as tf
 import tensorflow.keras.layers as layers
 from tensorflow.keras.utils import get_custom_objects
+from tensorflow.python.training.tracking import data_structures
+from tensorflow.python.keras.layers.recurrent import DropoutRNNCellMixin
+from tensorflow.python.keras.utils import tf_utils
+from tensorflow.keras import backend as K
+from tensorflow.python.util import nest
+from tensorflow.python.ops import array_ops
+from tensorflow.python.framework import tensor_shape
 
 
 ################################################################################
@@ -119,27 +126,27 @@ class GroupNormalization(tf.keras.layers.Layer):
     def get_config(self):
         config = {
             'groups':
-            self.groups,
+                self.groups,
             'axis':
-            self.axis,
+                self.axis,
             'epsilon':
-            self.epsilon,
+                self.epsilon,
             'center':
-            self.center,
+                self.center,
             'scale':
-            self.scale,
+                self.scale,
             'beta_initializer':
-            tf.keras.initializers.serialize(self.beta_initializer),
+                tf.keras.initializers.serialize(self.beta_initializer),
             'gamma_initializer':
-            tf.keras.initializers.serialize(self.gamma_initializer),
+                tf.keras.initializers.serialize(self.gamma_initializer),
             'beta_regularizer':
-            tf.keras.regularizers.serialize(self.beta_regularizer),
+                tf.keras.regularizers.serialize(self.beta_regularizer),
             'gamma_regularizer':
-            tf.keras.regularizers.serialize(self.gamma_regularizer),
+                tf.keras.regularizers.serialize(self.gamma_regularizer),
             'beta_constraint':
-            tf.keras.constraints.serialize(self.beta_constraint),
+                tf.keras.constraints.serialize(self.beta_constraint),
             'gamma_constraint':
-            tf.keras.constraints.serialize(self.gamma_constraint)
+                tf.keras.constraints.serialize(self.gamma_constraint)
         }
         base_config = super(GroupNormalization, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
@@ -190,8 +197,8 @@ class GroupNormalization(tf.keras.layers.Layer):
         dim = input_shape[self.axis]
         if dim is None:
             raise ValueError('Axis ' + str(self.axis) + ' of '
-                             'input tensor should have a defined dimension '
-                             'but the layer received an input with shape ' +
+                                                        'input tensor should have a defined dimension '
+                                                        'but the layer received an input with shape ' +
                              str(input_shape) + '.')
 
     def _set_number_of_groups_for_instance_norm(self, input_shape):
@@ -206,12 +213,14 @@ class GroupNormalization(tf.keras.layers.Layer):
         if dim < self.groups:
             raise ValueError(
                 'Number of groups (' + str(self.groups) + ') cannot be '
-                'more than the number of channels (' + str(dim) + ').')
+                                                          'more than the number of channels (' + str(
+                    dim) + ').')
 
         if dim % self.groups != 0:
             raise ValueError(
                 'Number of groups (' + str(self.groups) + ') must be a '
-                'multiple of the number of channels (' + str(dim) + ').')
+                                                          'multiple of the number of channels (' + str(
+                    dim) + ').')
 
     def _check_axis(self):
 
@@ -350,6 +359,7 @@ class InstanceNormalization(GroupNormalization):
         kwargs["groups"] = -1
         super(InstanceNormalization, self).__init__(**kwargs)
 
+
 ################################################################################
 # END OF THIEVERY
 ################################################################################
@@ -378,3 +388,306 @@ class ResidualBlock(layers.Layer):
         if self.shortcut:
             inputs = self.shortcut(inputs)
         return self.activation(inputs + interm)
+
+
+@register_keras_custom_object
+class LSTMCell(DropoutRNNCellMixin, layers.Layer):
+    """Cell class for the LSTM layer.
+    Arguments:
+      units: Positive integer, dimensionality of the output space.
+      activation: Activation function to use.
+        Default: hyperbolic tangent (`tanh`).
+        If you pass `None`, no activation is applied
+        (ie. "linear" activation: `a(x) = x`).
+      recurrent_activation: Activation function to use
+        for the recurrent step.
+        Default: hard sigmoid (`hard_sigmoid`).
+        If you pass `None`, no activation is applied
+        (ie. "linear" activation: `a(x) = x`).
+      use_bias: Boolean, whether the layer uses a bias vector.
+      kernel_initializer: Initializer for the `kernel` weights matrix,
+        used for the linear transformation of the inputs.
+      recurrent_initializer: Initializer for the `recurrent_kernel`
+        weights matrix,
+        used for the linear transformation of the recurrent state.
+      bias_initializer: Initializer for the bias vector.
+      unit_forget_bias: Boolean.
+        If True, add 1 to the bias of the forget gate at initialization.
+        Setting it to true will also force `bias_initializer="zeros"`.
+        This is recommended in [Jozefowicz et
+          al.](http://www.jmlr.org/proceedings/papers/v37/jozefowicz15.pdf)
+      kernel_regularizer: Regularizer function applied to
+        the `kernel` weights matrix.
+      recurrent_regularizer: Regularizer function applied to
+        the `recurrent_kernel` weights matrix.
+      bias_regularizer: Regularizer function applied to the bias vector.
+      kernel_constraint: Constraint function applied to
+        the `kernel` weights matrix.
+      recurrent_constraint: Constraint function applied to
+        the `recurrent_kernel` weights matrix.
+      bias_constraint: Constraint function applied to the bias vector.
+      dropout: Float between 0 and 1.
+        Fraction of the units to drop for
+        the linear transformation of the inputs.
+      recurrent_dropout: Float between 0 and 1.
+        Fraction of the units to drop for
+        the linear transformation of the recurrent state.
+      implementation: Implementation mode, either 1 or 2.
+        Mode 1 will structure its operations as a larger number of
+        smaller dot products and additions, whereas mode 2 will
+        batch them into fewer, larger operations. These modes will
+        have different performance profiles on different hardware and
+        for different applications.
+    Call arguments:
+      inputs: A 2D tensor.
+      states: List of state tensors corresponding to the previous timestep.
+      training: Python boolean indicating whether the layer should behave in
+        training mode or in inference mode. Only relevant when `dropout` or
+        `recurrent_dropout` is used.
+    """
+
+    def __init__(self,
+                 units,
+                 activation=tf.nn.tanh,
+                 recurrent_activation=tf.nn.sigmoid,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 recurrent_initializer='orthogonal',
+                 bias_initializer='zeros',
+                 unit_forget_bias=True,
+                 kernel_regularizer=None,
+                 recurrent_regularizer=None,
+                 bias_regularizer=None,
+                 kernel_constraint=None,
+                 recurrent_constraint=None,
+                 bias_constraint=None,
+                 dropout=0.,
+                 recurrent_dropout=0.,
+                 implementation=1,
+                 **kwargs):
+        super(LSTMCell, self).__init__(**kwargs)
+        self.units = units
+        self.activation = activation
+        self.recurrent_activation = recurrent_activation
+        self.use_bias = use_bias
+
+        self.kernel_initializer = tf.keras.initializers.get(kernel_initializer)
+        self.recurrent_initializer = tf.keras.initializers.get(
+            recurrent_initializer)
+        self.bias_initializer = tf.keras.initializers.get(bias_initializer)
+        self.unit_forget_bias = unit_forget_bias
+
+        self.kernel_regularizer = tf.keras.regularizers.get(kernel_regularizer)
+        self.recurrent_regularizer = tf.keras.regularizers.get(
+            recurrent_regularizer)
+        self.bias_regularizer = tf.keras.regularizers.get(bias_regularizer)
+
+        self.kernel_constraint = tf.keras.constraints.get(kernel_constraint)
+        self.recurrent_constraint = tf.keras.constraints.get(
+            recurrent_constraint)
+        self.bias_constraint = tf.keras.constraints.get(bias_constraint)
+
+        self.dropout = min(1., max(0., dropout))
+        self.recurrent_dropout = min(1., max(0., recurrent_dropout))
+        self.implementation = implementation
+        # tuple(_ListWrapper) was silently dropping list content in at least 2.7.10,
+        # and fixed after 2.7.16. Converting the state_size to wrapper around
+        # NoDependency(), so that the base_layer.__setattr__ will not convert it to
+        # ListWrapper. Down the stream, self.states will be a list since it is
+        # generated from nest.map_structure with list, and tuple(list) will work
+        # properly.
+        self.state_size = data_structures.NoDependency([self.units, self.units])
+        self.output_size = self.units
+
+    @tf_utils.shape_type_conversion
+    def build(self, input_shape):
+        input_dim = input_shape[-1]
+        self.kernel = self.add_weight(
+            shape=(input_dim, self.units * 4),
+            name='kernel',
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            constraint=self.kernel_constraint)
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units * 4),
+            name='recurrent_kernel',
+            initializer=self.recurrent_initializer,
+            regularizer=self.recurrent_regularizer,
+            constraint=self.recurrent_constraint)
+
+        if self.use_bias:
+            if self.unit_forget_bias:
+
+                def bias_initializer(_, *args, **kwargs):
+                    return K.concatenate([
+                        self.bias_initializer((self.units,), *args, **kwargs),
+                        tf.keras.initializers.Ones()((self.units,), *args,
+                                                     **kwargs),
+                        self.bias_initializer((self.units * 2,), *args,
+                                              **kwargs),
+                    ])
+            else:
+                bias_initializer = self.bias_initializer
+            self.bias = self.add_weight(
+                shape=(self.units * 4,),
+                name='bias',
+                initializer=bias_initializer,
+                regularizer=self.bias_regularizer,
+                constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        self.built = True
+
+    def _compute_carry_and_output(self, x, h_tm1, c_tm1):
+        """Computes carry and output using split kernels."""
+        x_i, x_f, x_c, x_o = x
+        h_tm1_i, h_tm1_f, h_tm1_c, h_tm1_o = h_tm1
+        i = self.recurrent_activation(
+            x_i + K.dot(h_tm1_i, self.recurrent_kernel[:, :self.units]))
+        f = self.recurrent_activation(x_f + K.dot(
+            h_tm1_f, self.recurrent_kernel[:, self.units:self.units * 2]))
+        c = f * c_tm1 + i * self.activation(x_c + K.dot(
+            h_tm1_c, self.recurrent_kernel[:, self.units * 2:self.units * 3]))
+        o = self.recurrent_activation(
+            x_o + K.dot(h_tm1_o, self.recurrent_kernel[:, self.units * 3:]))
+        return c, o
+
+    def _compute_carry_and_output_fused(self, z, c_tm1):
+        """Computes carry and output using fused kernels."""
+        z0, z1, z2, z3 = z
+        i = self.recurrent_activation(z0)
+        f = self.recurrent_activation(z1)
+        c = f * c_tm1 + i * self.activation(z2)
+        o = self.recurrent_activation(z3)
+        return c, o
+
+    def call(self, inputs, states, training=None):
+        h_tm1 = states[0]  # previous memory state
+        c_tm1 = states[1]  # previous carry state
+
+        dp_mask = self.get_dropout_mask_for_cell(inputs, training, count=4)
+        rec_dp_mask = self.get_recurrent_dropout_mask_for_cell(
+            h_tm1, training, count=4)
+
+        if self.implementation == 1:
+            if 0 < self.dropout < 1.:
+                inputs_i = inputs * dp_mask[0]
+                inputs_f = inputs * dp_mask[1]
+                inputs_c = inputs * dp_mask[2]
+                inputs_o = inputs * dp_mask[3]
+            else:
+                inputs_i = inputs
+                inputs_f = inputs
+                inputs_c = inputs
+                inputs_o = inputs
+            k_i, k_f, k_c, k_o = array_ops.split(
+                self.kernel, num_or_size_splits=4, axis=1)
+            x_i = K.dot(inputs_i, k_i)
+            x_f = K.dot(inputs_f, k_f)
+            x_c = K.dot(inputs_c, k_c)
+            x_o = K.dot(inputs_o, k_o)
+            if self.use_bias:
+                b_i, b_f, b_c, b_o = array_ops.split(
+                    self.bias, num_or_size_splits=4, axis=0)
+                x_i = K.bias_add(x_i, b_i)
+                x_f = K.bias_add(x_f, b_f)
+                x_c = K.bias_add(x_c, b_c)
+                x_o = K.bias_add(x_o, b_o)
+
+            if 0 < self.recurrent_dropout < 1.:
+                h_tm1_i = h_tm1 * rec_dp_mask[0]
+                h_tm1_f = h_tm1 * rec_dp_mask[1]
+                h_tm1_c = h_tm1 * rec_dp_mask[2]
+                h_tm1_o = h_tm1 * rec_dp_mask[3]
+            else:
+                h_tm1_i = h_tm1
+                h_tm1_f = h_tm1
+                h_tm1_c = h_tm1
+                h_tm1_o = h_tm1
+            x = (x_i, x_f, x_c, x_o)
+            h_tm1 = (h_tm1_i, h_tm1_f, h_tm1_c, h_tm1_o)
+            c, o = self._compute_carry_and_output(x, h_tm1, c_tm1)
+        else:
+            if 0. < self.dropout < 1.:
+                inputs *= dp_mask[0]
+            z = K.dot(inputs, self.kernel)
+            if 0. < self.recurrent_dropout < 1.:
+                h_tm1 *= rec_dp_mask[0]
+            z += K.dot(h_tm1, self.recurrent_kernel)
+            if self.use_bias:
+                z = K.bias_add(z, self.bias)
+
+            z = array_ops.split(z, num_or_size_splits=4, axis=1)
+            c, o = self._compute_carry_and_output_fused(z, c_tm1)
+
+        h = o * self.activation(c)
+        return h, [h, c]
+
+    def get_config(self):
+        config = {
+            'units':
+                self.units,
+            'activation':
+                tf.keras.activations.serialize(self.activation),
+            'recurrent_activation':
+                tf.keras.activations.serialize(self.recurrent_activation),
+            'use_bias':
+                self.use_bias,
+            'kernel_initializer':
+                tf.keras.initializers.serialize(self.kernel_initializer),
+            'recurrent_initializer':
+                tf.keras.initializers.serialize(self.recurrent_initializer),
+            'bias_initializer':
+                tf.keras.initializers.serialize(self.bias_initializer),
+            'unit_forget_bias':
+                self.unit_forget_bias,
+            'kernel_regularizer':
+                tf.keras.regularizers.serialize(self.kernel_regularizer),
+            'recurrent_regularizer':
+                tf.keras.regularizers.serialize(self.recurrent_regularizer),
+            'bias_regularizer':
+                tf.keras.regularizers.serialize(self.bias_regularizer),
+            'kernel_constraint':
+                tf.keras.constraints.serialize(self.kernel_constraint),
+            'recurrent_constraint':
+                tf.keras.constraints.serialize(self.recurrent_constraint),
+            'bias_constraint':
+                tf.keras.constraints.serialize(self.bias_constraint),
+            'dropout':
+                self.dropout,
+            'recurrent_dropout':
+                self.recurrent_dropout,
+            'implementation':
+                self.implementation
+        }
+        base_config = super(LSTMCell, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def get_initial_state(self, inputs=None, batch_size=None, dtype=None):
+        return list(_generate_zero_filled_state_for_cell(
+            self, inputs, batch_size, dtype))
+
+
+def _generate_zero_filled_state_for_cell(cell, inputs, batch_size, dtype):
+  if inputs is not None:
+    batch_size = array_ops.shape(inputs)[0]
+    dtype = inputs.dtype
+  return _generate_zero_filled_state(batch_size, cell.state_size, dtype)
+
+
+def _generate_zero_filled_state(batch_size_tensor, state_size, dtype):
+  """Generate a zero filled tensor with shape [batch_size, state_size]."""
+  if batch_size_tensor is None or dtype is None:
+    raise ValueError(
+        'batch_size and dtype cannot be None while constructing initial state: '
+        'batch_size={}, dtype={}'.format(batch_size_tensor, dtype))
+
+  def create_zeros(unnested_state_size):
+    flat_dims = tensor_shape.as_shape(unnested_state_size).as_list()
+    init_state_size = [batch_size_tensor] + flat_dims
+    return array_ops.zeros(init_state_size, dtype=dtype)
+
+  if nest.is_sequence(state_size):
+    return nest.map_structure(create_zeros, state_size)
+  else:
+    return create_zeros(state_size)
